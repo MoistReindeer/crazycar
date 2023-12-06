de7bfec0350bc73a45acf4cfd37b37bb8dc95d0a
Deutsch Tobias <tobias.deutsch@edu.fh-joanneum.at>
2023-12-06 14:23:02 +0100

    Updated timerB0 for ADC

--- HAL/hal_timerB0.c
+++ HAL/hal_timerB0.c
@@ -1,14 +1,10 @@
 #include <msp430.h>
-#include "HAL/hal_ucs.h"
-#include "HAL/hal_timerB0.h"
-
-#define T_FREQ 2
+#include <HAL/hal_ucs.h>
+#include <HAL/hal_timerB0.h>
 
 void HAL_TimerB0_Init(void) {
-    TB0CTL |= TBSSEL_2; //Setzt SMCLK als clock source
-    TB0CTL |= MC_1; //Setzt den Timer in den UP-Mode
-    TB0CTL |= ID_3; //Setzt den Input Divider zu /8
-    TB0CCTL0 |= CCIE; //Enabled den Capture Compare Interrupt
-    TB0EX0 |= TBIDEX_4; //Setzt den zweiten Divider zu /5
-    TB0CCR0 = SMCLK_FREQU/T_FREQ/8/5; //Berechnet den Wert, der ins Capture Compare Register kommt
+    TB0CTL |= TBSSEL__SMCLK;    //Setzt SMCLK als clock source
+    TB0CTL |= MC_1;             //Setzt den Timer in den UP-Mode
+    TB0CCTL0 |= CCIE;           //Enabled den Capture Compare Interrupt
+    TB0CCR0 = 20833;            // Wert für eine Periodendauer von 120Hz
 }
