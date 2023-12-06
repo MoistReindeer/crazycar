06abfeaeb46ccf95d4140fe49db1b844474a6672
Deutsch Tobias <tobias.deutsch@edu.fh-joanneum.at>
2023-12-06 15:47:38 +0100

    Updated timer B0

--- HAL/hal_timerB0.c
+++ HAL/hal_timerB0.c
@@ -6,5 +6,7 @@ void HAL_TimerB0_Init(void) {
     TB0CTL |= TBSSEL__SMCLK;    //Setzt SMCLK als clock source
     TB0CTL |= MC_1;             //Setzt den Timer in den UP-Mode
     TB0CCTL0 |= CCIE;           //Enabled den Capture Compare Interrupt
+    TB0CCTL1 |= OUTMOD_6;       // Einzelner Puls
     TB0CCR0 = 20833;            // Wert für eine Periodendauer von 120Hz
+    TB0CCR1 = 1;                // Setzt für einen Puls als trigger
 }
